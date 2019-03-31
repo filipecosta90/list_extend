508dd9eb98f47a8809b7505f00675f9020466f2a
fcosta_oliveira <filipecosta90@gmail.com>
2019-03-31 10:21:50 -0700

    [add] cleaned code

--- list_extend.c
+++ list_extend.c
@@ -1,16 +1,17 @@
- /*************************************************************************
- * 
- * Redis 5.X under the hood: 3 - Writing a Redis Module
- * ____________________________________________________
- * 
- *  [2019] @fcosta_oliveira 
- *  
- */
+/*************************************************************************
+*
+* Redis 5.X under the hood: 3 - Writing a Redis Module
+* ____________________________________________________
+*
+*  [2019] @fcosta_oliveira
+*
+*/
 
 #include "redismodule.h"
 #include <limits.h>
 
-int ListExtendFilter_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+int ListExtendFilter_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
+                                  int argc) {
 
   // LIST_EXTEND.FILTER source_list destination_list low_value high_value
   if (argc != 5) {
@@ -19,7 +20,8 @@ int ListExtendFilter_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
   RedisModule_AutoMemory(ctx);
 
   // Open field/value list keys
-  RedisModuleKey *sourceListKey = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
+  RedisModuleKey *sourceListKey =
+      RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
   int sourceListKeyType = RedisModule_KeyType(sourceListKey);
 
   if (sourceListKeyType != REDISMODULE_KEYTYPE_LIST &&
@@ -29,7 +31,8 @@ int ListExtendFilter_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
   }
 
   // Open destination key
-  RedisModuleKey *destinationListKey = RedisModule_OpenKey(ctx, argv[2], REDISMODULE_WRITE);
+  RedisModuleKey *destinationListKey =
+      RedisModule_OpenKey(ctx, argv[2], REDISMODULE_WRITE);
   RedisModule_DeleteKey(destinationListKey);
 
   // Get length of list
@@ -40,49 +43,58 @@ int ListExtendFilter_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
     return REDISMODULE_OK;
   }
 
-  /* Rotate and increment. */
-  size_t added = 0;
-  for (size_t pos = 0; pos < sourceListLength; pos++) {
-    RedisModuleString *ele = RedisModule_ListPop(sourceListKey, REDISMODULE_LIST_TAIL);
-    RedisModule_ListPush(sourceListKey, REDISMODULE_LIST_HEAD, ele);
-    char strLowerLimit[] = "-inf";
-    char strUpperLimit[] = "+inf";
-    long long val;
-    long long lowerLimit;
-    long long upperLimit;
-    RedisModuleString *expectedMinusInf, *expectedPlusInf;
-    expectedMinusInf = RedisModule_CreateString(ctx, strLowerLimit, 4);
-    expectedPlusInf = RedisModule_CreateString(ctx, strUpperLimit, 4);
-    int lowerLimitOk = 0;
-    int upperLimitOk = 0;
-    char *eptr;
+  char strLowerLimit[] = "-inf";
+  char strUpperLimit[] = "+inf";
+  RedisModuleString *expectedMinusInf, *expectedPlusInf;
+  long long lowerLimit;
+  long long upperLimit;
+  int lowerLimitOk = 0;
+  int upperLimitOk = 0;
 
-    lowerLimit = strtol(RedisModule_StringPtrLen(argv[3], NULL), &eptr, 10);
+  expectedMinusInf = RedisModule_CreateString(ctx, strLowerLimit, 4);
+  expectedPlusInf = RedisModule_CreateString(ctx, strUpperLimit, 4);
 
-    if (RedisModule_StringCompare(argv[3], expectedMinusInf) == 0) {
-      lowerLimit = LONG_MIN;
+  // check if argv[3] is -inf
+  if (RedisModule_StringCompare(argv[3], expectedMinusInf) == 0) {
+    lowerLimit = LONG_MIN;
+    lowerLimitOk = 1;
+  } else {
+    if (RedisModule_StringToLongLong(argv[3], &lowerLimit) == REDISMODULE_OK)
       lowerLimitOk = 1;
-    } else {
-      if (RedisModule_StringToLongLong(argv[3], &lowerLimit) == REDISMODULE_OK)
-        lowerLimitOk = 1;
-    }
+  }
 
-    if (RedisModule_StringCompare(argv[4], expectedPlusInf) == 0) {
-      upperLimit = LONG_MAX;
+  // check if argv[4] is +inf
+  if (RedisModule_StringCompare(argv[4], expectedPlusInf) == 0) {
+    upperLimit = LONG_MAX;
+    upperLimitOk = 1;
+  } else {
+    if (RedisModule_StringToLongLong(argv[4], &upperLimit) == REDISMODULE_OK)
       upperLimitOk = 1;
-    } else {
-      if (RedisModule_StringToLongLong(argv[4], &upperLimit) == REDISMODULE_OK)
-        upperLimitOk = 1;
-    }
+  }
+
+  // the number of added elements to the destination_list
+  size_t added = 0;
+  // loop trough source_list
+  for (size_t pos = 0; pos < sourceListLength; pos++) {
+    RedisModuleString *ele =
+        RedisModule_ListPop(sourceListKey, REDISMODULE_LIST_TAIL);
+    RedisModule_ListPush(sourceListKey, REDISMODULE_LIST_HEAD, ele);
+
+    long long val;
+
+    // convert string to long long
     if ((RedisModule_StringToLongLong(ele, &val) == REDISMODULE_OK) &&
         lowerLimitOk == 1 && upperLimitOk == 1) {
       if (val >= lowerLimit && val <= upperLimit) {
-        
-        // RedisModule_Log(ctx, "notice", "element within range %d %d %d", lowerLimit, val, upperLimit );
-        RedisModuleString *newele = RedisModule_CreateStringFromLongLong(ctx, val);
+
+        // RedisModule_Log(ctx, "notice", "element within range %d %d %d",
+        // lowerLimit, val, upperLimit );
+        RedisModuleString *newele =
+            RedisModule_CreateStringFromLongLong(ctx, val);
 
         // push to destination_list
-        if (RedisModule_ListPush(destinationListKey, REDISMODULE_LIST_HEAD, newele) == REDISMODULE_ERR) {
+        if (RedisModule_ListPush(destinationListKey, REDISMODULE_LIST_HEAD,
+                                 newele) == REDISMODULE_ERR) {
           return REDISMODULE_ERR;
         }
         added++;
@@ -98,13 +110,18 @@ int ListExtendFilter_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
   return REDISMODULE_OK;
 }
 
-int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  if (RedisModule_Init(ctx, "list_extend", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
+int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv,
+                       int argc) {
+  if (RedisModule_Init(ctx, "list_extend", 1, REDISMODULE_APIVER_1) ==
+      REDISMODULE_ERR)
     return REDISMODULE_ERR;
 
   // "write": The command may modify the data set (it may also read from it).
-  // "deny-oom": The command may use additional memory and should be denied during out of memory conditions.
-  if (RedisModule_CreateCommand(ctx, "list_extend.filter", ListExtendFilter_RedisCommand, "write deny-oom", 1, 1, 1) == REDISMODULE_ERR)
+  // "deny-oom": The command may use additional memory and should be denied
+  // during out of memory conditions.
+  if (RedisModule_CreateCommand(ctx, "list_extend.filter",
+                                ListExtendFilter_RedisCommand, "write deny-oom",
+                                1, 1, 1) == REDISMODULE_ERR)
     return REDISMODULE_ERR;
 
   return REDISMODULE_OK;
